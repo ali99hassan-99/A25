3c0dc8e37c6ec0deac54bcc6224f7dbb3ef34c00
ali99hassan-99 <falah07822ali@gmail.com>
2025-09-15 17:32:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,19 @@
-const CACHE_NAME = 'debt-ledger-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-// تثبيت Service Worker وتخزين الملفات
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
     })
   );
 });
 
-// تفعيل Service Worker وحذف الكاش القديم
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-    ))
-  );
-});
-
-// جلب الملفات من الكاش أو الشبكة
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
